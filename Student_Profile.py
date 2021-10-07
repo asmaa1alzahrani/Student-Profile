df086b1c2d0eac86ff365ea6e4d5fe9c50212757
asmaa1alzahrani <80360134+asmaa1alzahrani@users.noreply.github.com>
2021-10-07 22:21:33 +0300

    Add files via upload

---
+++ Student_Profile.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 10/7/2021
+# 
+# Asmaa Alzahrani 
+# 
+# cohort 1
+# 
+# ## Student career goal statement 
+# 
+# -Work in Bank or fintech company, and become a Data   Analyst 
+# 
+# -I would like to work with The National Commercial   Bank 
+# 
+# -I want to financial data analysing , visulaization
+# 
+# -Iwant to contribut to making smarter decision ,using programming language.
+# 
+# ## Student mini-CV:
+# 
+# -Degrees Completed:
+#     BSc Computer Science 
+# 
+# -Top 3 Technical skills:
+#     Excel , SQL, Visulization tools
+#     
+# -Top 3 Interest in Fintevh:
+#     Algorithm Trading, Blockchain , Smart Contract
+# 
+
+# 
